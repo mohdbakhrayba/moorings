2fb5a0d85380d07ea1b3372b2bb1b8e9f3fbc670
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-09-20 12:23:03 +0800

    Align BASE_DIR with ledger

--- mooring/wsgi.py
+++ mooring/wsgi.py
@@ -9,6 +9,7 @@ from dj_static import Cling, MediaCling
 import confy
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 confy.read_environment_file(BASE_DIR+"/.env")
+os.environ.setdefault("BASE_DIR", BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mooring.settings")
 #application = get_wsgi_application()
