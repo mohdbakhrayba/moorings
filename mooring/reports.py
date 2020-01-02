f4119637e770d44a7dc9cb14f2ec8c8c0746eaaf
jason.moore@dbca.wa.gov.au <Jason Moore>
2020-01-02 15:27:05 +0800

    Report Fixes

--- mooring/reports.py
+++ mooring/reports.py
@@ -196,7 +196,7 @@ def bookings_report(_date):
         history_bookings = BookingHistory.objects.filter(created__date=_date).exclude(booking__booking_type=3)
 
         strIO = StringIO()
-        fieldnames = ['Date','Confirmation Number','Name','Invoice Total','Override Price','Override Reason','Override Details','Invoice','Booking Type','Created By']
+        fieldnames = ['Date','Confirmation Number','Name','Invoice Total','Override Reason','Override Details','Invoice','Booking Type','Created By']
         writer = csv.writer(strIO)
         writer.writerow(fieldnames)
 
@@ -210,8 +210,11 @@ def bookings_report(_date):
             created_by =''
             if b.created_by is not None: 
                  created_by = b.created_by
- 
-            writer.writerow([created.strftime('%d/%m/%Y %H:%M:%S'),b.confirmation_number,b_name.encode('utf-8'),b.active_invoice.amount if b.active_invoice else '',b.override_price,b.override_reason,b.override_reason_info,b.active_invoice.reference if b.active_invoice else '', types[b.booking_type] if b.booking_type in types else b.booking_type, created_by])
+
+            reason_text = ''
+            if b.override_reason:
+                 reason_text = b.override_reason.text 
+            writer.writerow([created.strftime('%d/%m/%Y %H:%M:%S'),b.confirmation_number,b_name.encode('utf-8'),b.active_invoice.amount if b.active_invoice else '',reason_text,b.override_reason_info,b.active_invoice.reference if b.active_invoice else '', types[b.booking_type] if b.booking_type in types else b.booking_type, created_by])
 
         for b in admission_bookings:
             b_name = 'No Name'
